1dde8f3924d438176e628e729c26c0d975c59aa2
Luke McFarlane <luke.mcfarlane@trineo.co.nz>
2014-12-29 16:33:02 +1300

    Facebook share dialog & open graph meta tags

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
+var OBJECT_TO_LIKE = 'http://lukemcfarlane.github.io/skymachine/';
+var FB_APP_ID = '315954451937355';
+
 function share() {
-    enableDownload();
+    FB.ui({
+        method: 'share',
+        href: OBJECT_TO_LIKE 
+    }, function(response){
+        console.log('Response:', response);
+    });
 }
 
 function enableDownload() {
@@ -20,20 +28,22 @@ $(document).ready(function() {
 /*****************************************************
  * INIT THE FACEBOOK SDK
  *****************************************************/
-// window.fbAsyncInit = function() {
-//     FB.init({
-//       appId      : '315954451937355',
-//       xfbml      : true,
-//       version    : 'v2.2'
-//     });
-// };
+window.fbAsyncInit = function() {
+    FB.init({
+      appId      : FB_APP_ID,
+      status     : true,
+      cookie     : true,
+      xfbml      : true,
+      version    : 'v2.0'
+    });
+};
 
-// (function(d, s, id){
-//     var js, fjs = d.getElementsByTagName(s)[0];
-//     if (d.getElementById(id)) {return;}
-//     js = d.createElement(s); js.id = id;
-//     js.src = "//connect.facebook.net/en_US/sdk.js";
-//     fjs.parentNode.insertBefore(js, fjs);
-// }(document, 'script', 'facebook-jssdk'));
+(function(d, s, id){
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) {return;}
+    js = d.createElement(s); js.id = id;
+    js.src = "//connect.facebook.net/en_US/sdk.js";
+    fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
 /******************************************************/
 
