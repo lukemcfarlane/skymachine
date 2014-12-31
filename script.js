cdfd4f1c8b92c270daae6ad88a3c1f76bba921e3
Luke McFarlane <luke.mcfarlane@trineo.co.nz>
2014-12-31 13:25:13 +1300

    Moved code from script into new class 'Skymachine'

--- script.js
+++ script.js
@@ -2,70 +2,15 @@ var OBJECT_TO_LIKE = 'http://lukemcfarlane.github.io/skymachine/';
 var FB_APP_ID = '315954451937355';
 var DL_URL = '/Getaway.zip';
 
-function login() {
-    FB.login(function(response) {
-        if (response.authResponse) {
-            console.log('Welcome!  Fetching your information.... ');
-            FB.api('/me', function(response) {
-                console.log('Good to see you, ' + response.name + '.');
-            });
-            share();
-        } else {
-            console.log('User cancelled login or did not fully authorize.');
-        }
-    }, {
-        scope: 'publish_actions',
-        return_scopes: true
-    });
-}
-
-function share() {
-    FB.ui({
-        method: 'share',
-        href: OBJECT_TO_LIKE
-    }, function(response){
-        debugger
-    });
-}
-
-function enableDownload() {
-    $('.share-link').hide();
-    $('p.dl-confirm').show();
-    setTimeout(function() {
-        window.location.href = DL_URL;
-    }, 1 * 1000);
-}
-
-function init() {
-    $('.share-link').click(function() {
-        // login();
-        enableDownload();
-    });
-}
 
 $(document).ready(function() {
+    window.Skymachine = new Skymachine(
+        OBJECT_TO_LIKE,
+        FB_APP_ID,
+        DL_URL
+    );
+
+    function init() {
+    };
     init();
 });
-
-/*****************************************************
- * INIT THE FACEBOOK SDK
- *****************************************************/
-window.fbAsyncInit = function() {
-    FB.init({
-      appId      : FB_APP_ID,
-      status     : true,
-      cookie     : true,
-      xfbml      : true,
-      version    : 'v2.0'
-    });
-};
-
-(function(d, s, id){
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) {return;}
-    js = d.createElement(s); js.id = id;
-    js.src = "//connect.facebook.net/en_US/sdk.js";
-    fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
-/******************************************************/
-
---
+++ skymachine.js
@@ -0,0 +1,79 @@
+var Skymachine = function(urlToShare, fbAppId, downloadUrl) {
+    this.urlToShare = urlToShare; 
+    this.fbAppId = fbAppId; 
+    this.downloadUrl = downloadUrl;
+};
+
+Skymachine.prototype.initFacebook = function() {
+    window.fbAsyncInit = function() {
+        FB.init({
+            appId      : this.fbAppId,
+            status     : true,
+            cookie     : true,
+            xfbml      : true,
+            version    : 'v2.0'
+        });
+    };
+
+    (function(d, s, id){
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) {return;}
+        js = d.createElement(s); js.id = id;
+        js.src = "//connect.facebook.net/en_US/sdk.js";
+        fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+};
+
+/**
+ * This function is not currently being used, as it requires the publish_actions
+ * permission to be approved for our Facebook app.
+ */
+Skymachine.prototype.login = function(callbackFn) {
+    FB.login(function(response) {
+        if (response.authResponse) {
+            FB.api('/me', function(response) {
+                callbackFn('Good to see you, ' + response.name + '.');
+            });
+        } else {
+            callbackFn('User cancelled login or did not fully authorize.');
+        }
+    }, {
+        scope: 'publish_actions',
+        return_scopes: true
+    });
+};
+
+/**
+ * Show a Facebook share dialog and call the callback function on
+ * complete. 
+ *
+ * Note: The response currently does not tell us whether or not the
+ *       user actually completed the share successfully. That is,
+ *       they could just click cancel or close the dialog and we will
+ *       get the same response.
+ *
+ *       To determine success would require the user to have logged 
+ *       in using the login function, as well as have the publish_actions 
+ *       permission approved for our app on Facebook.
+ */
+Skymachine.protoype.share = function(callbackFn) {
+    FB.ui({
+        method: 'share',
+        href: this.urlToShare 
+    }, function(response){
+        callbackFn(response);
+    });
+};
+
+/**
+ * Automatically start the file download.
+ * 
+ * Note: The file should be a zip file for it to actually download,
+ *       otherwise some browsers will just open the mp3 file in the
+ *       browser itself.
+ */
+Skymachine.protype.startDownload = function() {
+    setTimeout(function() {
+        window.location.href = this.downloadUrl;
+    }, 1 * 1000); // Start downloading automatically in 1 second
+};
