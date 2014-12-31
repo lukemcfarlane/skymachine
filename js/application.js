5b82fb0a447666c135eab3785b4efd7bb80b1e58
Luke McFarlane <luke.mcfarlane@trineo.co.nz>
2014-12-31 13:57:56 +1300

    Hook up share button to handler

--- js/application.js
+++ js/application.js
@@ -2,15 +2,21 @@ var OBJECT_TO_LIKE = 'http://lukemcfarlane.github.io/skymachine/';
 var FB_APP_ID = '315954451937355';
 var DL_URL = '/files/Getaway.zip';
 
-
-$(document).ready(function() {
+function init() {
     window.Skymachine = new Skymachine(
         OBJECT_TO_LIKE,
         FB_APP_ID,
         DL_URL
     );
 
-    function init() {
-    };
+    debugger
+    $('#share-link').click(function() {
+        Skymachine.share(function() {
+            Skymachine.startDownload();
+        });
+    });
+};
+
+$(document).ready(function() {
     init();
 });
